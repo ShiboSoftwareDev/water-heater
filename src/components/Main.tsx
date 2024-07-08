8b4bc88112e4d4334dc0b4423a419de5d31aa3c6
Ahmed <ahmedsheb2003@yahoo.com>
2024-07-08 21:47:02 +0200

    added thermocouple

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,16 +1,8 @@
 import React from "react";
-import Heater from "./Heater";
 import Temp from "./Temp";
 
 const Main = () => {
-  const [page, setPage] = React.useState("temp");
-  console.log(page);
-  return (
-    <>
-      {page === "heater" && <Heater setPage={setPage} />}
-      {page === "temp" && <Temp setPage={setPage} />}
-    </>
-  );
+  return <Temp />;
 };
 
 export default Main;
--- src/components/Temp.tsx
+++ src/components/Temp.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
+import Draggable, { DraggableData } from "react-draggable";
 
-const Temp = ({
-  setPage,
-}: {
-  setPage: React.Dispatch<React.SetStateAction<string>>;
-}) => {
+const Temp = () => {
+  const [inRange, setInRange] = useState(false);
+  const [draggable, setDraggble] = useState(true);
   const [water, setWater] = useState<{
     temperature: number;
     setPoint: 70;
@@ -41,68 +40,34 @@ const Temp = ({
     if (water.temperature > water.setPoint) window.electronAPI.playSound(true);
     else window.electronAPI.playSound(false);
   }, [water.temperature]);
+  useEffect(() => {
+    if (inRange && heater.state === false) {
+      setHeater((prev) => ({ ...prev, state: true }));
+    } else if (heater.state === true && !inRange)
+      setHeater((prev) => ({ ...prev, state: false }));
+  }, [draggable]);
+
+  const eventLogger = (e: MouseEvent, data: DraggableData) => {
+    if (!draggable) return;
+    setDraggble(false);
+    setTimeout(() => {
+      setDraggble(true);
+    }, 100);
+    setInRange(data.x > 770 && data.y < -213 && data.x < 1020 && data.y > -276);
+    console.log(data.x);
+    console.log(data.y);
+  };
   return (
     <>
-      <div className=" h-screen w-screen items-center bg-black relative">
-        <div className="flex flex-col  justify-center  items-center">
-          <h1
-            onClick={() => setPage("heater")}
-            className="hover:cursor-pointer w-fit h-fit pt-8 text-white text-6xl font-extrabold text-center mb-9 font-mono"
-          >
-            {`<--`} Water heater
-          </h1>
-        </div>
-        <div className="flex justify-center flex-col items-center">
-          {water.temperature > water.setPoint ? (
-            <div>
-              <img
-                className="h-20 ml-2 animate-custom -mb-8"
-                src="../../assets/pics/f.png"
-                alt=""
-              />
-              <img
-                className="h-72"
-                src="../../assets/pics/fire_detector.png"
-                alt=""
-              />
-            </div>
-          ) : (
-            <div>
-              <img
-                className="h-20 animate-custom -mb-8"
-                src="../../assets/pics/.png"
-                alt=""
-              />
-              <img
-                className="h-72"
-                src="../../assets/pics/fire_detector2.png"
-                alt=""
-              />
-            </div>
-          )}
-        </div>
-        {water.temperature > water.setPoint && (
-          <img
-            className="h-40 w-40 absolute top-[20%] left-[80%] animate-custom rotate-"
-            src="../../assets/pics/alarm.png"
-            alt=""
-          />
-        )}
-        <div
-          className={` rounded-full  h-28 w-28 absolute top-[20%] left-[30%] pt-6   text-white text-6xl font-extrabold text-center font-mono ${
-            water.temperature > water.setPoint
-              ? "animate-custom bg-red-400"
-              : "bg-green-400"
-          }`}
-        >
-          {water.temperature}
-        </div>
-        <form className="  border-4 rounded-3xl pb-16 w-52 flex justify-center items-center absolute top-[250px] left-[70px] flex-col ">
-          <label htmlFor="temperature" className="text-white pb-2 pt-4">
-            Temperature (°C)
+      <div className="bg-[url('../../assets/pics/image.png')] ml-[650px] bg-contain bg-no-repeat h-[300px] -mb-20 w-[500px]"></div>
+
+      <div className="flex flex-row w-min gap-5 ml-80 border z-10 border-black bg-slate-500 p-2">
+        <form className="border border-black h-28 w-52 flex justify-center items-center top-[250px] left-[70px] flex-col">
+          <label htmlFor="temperature" className="pb-2 pt-4">
+            Temperature control (°C)
           </label>
           <input
-            className="h-8 w-40 items-center text-center"
+            className="h-8 w-40 bg-slate-300 rounded-lg items-center text-center"
             type="number"
             value={water.temperature}
             onChange={(e) =>
@@ -113,43 +78,30 @@ const Temp = ({
             }
           />
         </form>
-        <button
-          className="hover:cursor-pointer border-2 bg-red-500 rounded-3xl w-44 hover:bg-red-800 h-fit absolute top-[60%] left-[5%]  text-white  font-extrabold text-center mb-9 font-mono"
-          onClick={() => setHeater((prev) => ({ ...prev, state: !prev.state }))}
+        <div
+          className={`rounded-full h-28 w-28 top-[20%] left-[30%] pt-6 text-6xl font-extrabold text-center font-mono ${
+            water.temperature > water.setPoint
+              ? "animate-custom bg-red-400"
+              : "bg-green-400"
+          }`}
+        ></div>
+        <div
+          className={`h-28 w-28 top-[20%] left-[30%] pt-6 text-6xl font-extrabold text-center font-mono border border-black underline`}
         >
-          Fire
-        </button>
-        {heater.state && (
-          <div className="flex justify-center align-bottom  absolute left-[730px] top-[800px]">
-            <div className="flex flex-row justify-center">
-              <div className="container">
-                <div className="red flame"></div>
-                <div className="orange flame"></div>
-                <div className="yellow flame"></div>
-                <div className="white flame"></div>
-              </div>
-              <div className="container2 -ml-2 mt-3">
-                <div className="red flame"></div>
-                <div className="orange flame"></div>
-                <div className="yellow flame"></div>
-                <div className="white flame"></div>
-              </div>
-              <div className="container2 -ml-2 mt-4">
-                <div className="red flame"></div>
-                <div className="orange flame"></div>
-                <div className="yellow flame"></div>
-                <div className="white flame"></div>
-              </div>
-              <div className="container2 -ml-2 mt-1">
-                <div className="red flame"></div>
-                <div className="orange flame"></div>
-                <div className="yellow flame"></div>
-                <div className="white flame"></div>
-              </div>
-            </div>
-          </div>
-        )}
+          {water.temperature}
+        </div>
       </div>
+
+      <Draggable onDrag={eventLogger}>
+        <img className="h-48 w-48" src="../../assets/pics/lighter.png" alt="" />
+      </Draggable>
+      {water.temperature > water.setPoint && (
+        <img
+          className="h-40 w-40 absolute bottom-[25%] left-[45%] animate-custom rotate-"
+          src="../../assets/pics/alarm.png"
+          alt=""
+        />
+      )}
     </>
   );
 };
